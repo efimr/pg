c6b21249f8941abe3c35bf743f99c6e50b1ee2eb
efimr <156814781+efimr@users.noreply.github.com>
2025-10-19 13:41:13 +0300

    queries added for testing
    Queries added for testing
    Added information parameter about the parsing process

--- public.url_parser.sql
+++ public.url_parser.sql
@@ -2,14 +2,24 @@ CREATE OR REPLACE FUNCTION public.url_parser(in_obj jsonb)
  RETURNS jsonb
  LANGUAGE plpgsql
 AS $function$
--- 2025-10-19 13:00 - v2
+-- 2025-10-19 13:00 - v3
 -- !!! feature under development !!!
+-- select public.url_parser(null::jsonb)
+-- select public.url_parser('{}'::jsonb)
 -- select public.url_parser('{"full_url": null}'::jsonb)
 -- select public.url_parser('{"full_url": ""}'::jsonb)
 declare
-  --x_full_url text := (in_obj ->> 'full_url')::int8;
+  x_info text := '';
+  x_full_url text;
   x_result jsonb := '{}'::jsonb;
 begin
+  x_full_url := (in_obj ->> 'full_url')::text;
+  
+  if x_full_url is null or x_full_url = '' then
+    x_info := 'unable to parse address;';
+    x_result := jsonb_build_object( 'info', x_info );
+  end if;
+
   /* we supplement the incoming object with parameters */
   x_result := in_obj || x_result;
   return x_result;
